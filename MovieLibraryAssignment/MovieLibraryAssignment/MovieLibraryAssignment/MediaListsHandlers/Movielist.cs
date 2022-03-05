7a2197c28c43fba07040e34ac207613d918f0363
kouvang322 <kouvang322@hotmail.com>
2022-03-05 17:35:07 -0600

    got rid of some comments

--- MovieLibraryAssignment/MovieLibraryAssignment/MovieLibraryAssignment/MediaListsHandlers/Movielist.cs
+++ MovieLibraryAssignment/MovieLibraryAssignment/MovieLibraryAssignment/MediaListsHandlers/Movielist.cs
@@ -87,37 +87,4 @@ namespace MovieLibraryAssignment
         }
 
     }
-
-    //public void ReadMovieFromFile()
-    //{
-    //    // these would be global 
-    //    List<Movie> movies = new List<Movie>();
-
-    //    //loop
-    //        {
-    //        // read line from file
-    //        // parse (id, title, genres)
-    //        Movie movie = new Movie();
-    //        movie.Id = id;
-    //        movie.Title = title;
-    //        movie.Genres[0] = dasdfasd;
-
-    //        // save to movies list
-    //        movies.Add(movie);
-    //    }
-
-    //    // could display here
-
 }
-
-//movies.Add(new Movie() { ID = 1, Title = "Toy Story(1995)", Genres = "Adventure, Animation, Children, Comedy, Fantasy" });
-
-//movies.Add(new Movie() { ID = 2, Title = "Jumanji(1995)", Genres = "Adventure, Children, Fantasy" });
-//movies.Add(new Movie() { ID = 3, Title = "Grumpier Old Men(1995)", Genres = "Comedy, Romance" });
-//movies.Add(new Movie() { ID = 4, Title = "Waiting to Exhale(1995)", Genres = "Comedy, Drama, Romance" });
-//movies.Add(new Movie() { ID = 5, Title = "Father of the Bride Part II(1995)", Genres = "Comedy" });
-//movies.Add(new Movie() { ID = 6, Title = "Heat(1995)", Genres = "Action, Crime, Thriller" });
-//movies.Add(new Movie() { ID = 7, Title = "Sabrina(1995)", Genres = "Comedy, Romance" });
-//movies.Add(new Movie() { ID = 8, Title = "Tom and Huck(1995)", Genres = "Adventure, Children" });
-//movies.Add(new Movie() { ID = 9, Title = "Sudden Death(1995)", Genres = "Action" });
-//movies.Add(new Movie() { ID = 10, Title = "GoldenEye(1995)", Genres = "Adventure, Action, Thriller" });
